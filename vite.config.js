96b9bc93860cccec185206ea6484999614ae914f
Mati <matidezi@gmail.com>
2025-11-10 13:45:52 +0100

    Configuro Vite para múltiples páginas (index + menu)

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,19 @@
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
-export default defineConfig({plugins: [tailwindcss()] });
+
+export default defineConfig({
+  plugins: [tailwindcss()],
+  root: '.', // raíz del proyecto
+  build: {
+    outDir: 'dist', // carpeta de salida
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        menu: 'menu.html',
+      },
+    },
+  },
+  server: {
+    open: '/index.html',
+  },
+});
